279e8d22d873a3e674aecf6a22b9af317ee77fb6
samuelliechty <samuelliechty@users.noreply.github.com>
2021-07-29 13:05:37 -0500

    Site Version One

--- src/components/Home.component.js
+++ src/components/Home.component.js
@@ -1,31 +1,85 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import CSS from './HomeStyling.component.css'
+import SwansonVideo from './video/swansonSiteEdit.mp4'
+import SwansonMiniClip from './video/swansonMiniClipWithFade.mp4'
+import SwansonMiniThinClip from './video/swansonMiniClipThin.mp4'
 
 function HomePage() {
     return (
         <>
             <div className="home-page">
                 <div className = "spacer"/>
-                <img
-                    className="imageTest"
-                    src="http://placehold.it/3840x2160"
-                    alt="4k image placeholder"
-                />
+                <video autoPlay loop muted>
+                    <source src={SwansonVideo} type ="video/mp4"/>
+                    <source src={SwansonMiniClip} type ="video/mp4"/>
+                </video>
+            
                 <div className="seller-one">
                     <div className = "sub-seller-one">
                         <h1>Professional Grade Videography</h1>
                         <h2>Presented In Beautiful 4K Resolution</h2>
                     </div>
                 </div>
-                <div className = "contentOne">
+
+                
+                
+                <video autoPlay loop muted>
+                    <source src={SwansonMiniClip} type ="video/mp4"/>
+                </video>
+
+                <video autoPlay loop muted>
+                    <source src={SwansonMiniThinClip} type ="video/mp4"/>
+                </video>
+
+                <div className = "collection">
+                <div className="seller-two">
+                    <div className = "sub-seller-two">
+                        <h1>His Image Studio</h1>
+                        <h2>Meets Your Video Needs</h2>
+                    </div>
+                </div>
+                
+                <div className="seller-three">
+                    <a href="https://www.w3schools.com">
+                    <div className = "sub-seller-three">
+                        <h2>Contact for Commission</h2>
+                    </div>
+                    </a>
+                </div>
 
                 </div>
-                <img
-                        className="imageTest"
-                        src="http://placehold.it/3840x2160"
-                        alt="4k image placeholder"
-                    />
+
+                <div className="seller-four">
+                    <h1>Explore More of Our Content</h1>
+                    <ul>
+                        <a href ="https://www.w3schools.com">
+                        <li className = "sub-seller-four">
+                            <h2>Real Estate</h2>
+                        </li>
+                        </a>
+
+                        <a href ="https://www.w3schools.com">
+                        <li className = "sub-seller-four">
+                            <h2>Construction</h2>
+                        </li>
+                        </a>
+
+                        <a href ="https://www.w3schools.com">
+                        <li className = "sub-seller-four">
+                            <h2>Special Events</h2>
+                        </li>
+                        </a>
+
+                        <a href ="https://www.w3schools.com">
+                        <li className = "sub-seller-four">
+                            <h2>Just For Fun</h2>
+                        </li>
+                        </a>
+                            
+                    </ul>
+                </div>
+                
             </div>
         </>
     )
