e7268b7110f4b18cd7a0f150968e2b44d11a8142
samuelliechty <samuelliechty@users.noreply.github.com>
2021-09-24 22:26:18 -0400

    Change to header on construction and inspections page

--- src/components/Construction.component.js
+++ src/components/Construction.component.js
@@ -9,7 +9,7 @@ function ConstructionPage(){
             <div className="construction-spacer"></div>
 
             <div className="construction-heading">
-                <h1>Welcome to His Image Studio Construction Services</h1>
+                <h1>Welcome to His Image Studio Construction and Inspectiong Services</h1>
                 <span>Lorem Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
             </div>
 
